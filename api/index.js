9e88e870cafb0898354e86c84584fde82fe420ce
suyanne <suyannesara17@gmail.com>
2025-04-02 17:10:29 -0300

    register route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const express = require("express");
+const bcrypt = require('bcrypt');
 const connectToDb = require("./database");
 const UrlInfo = require("./models/UrlInfo");
+const User = require("./models/User");
 var bodyParser = require("body-parser");
 
 //connect to database
@@ -22,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// //Routes
+//Routes
 
 app.get("/", async (req, res) => {
   const shortUrls = await UrlInfo.find();
@@ -63,6 +65,34 @@ app.get("/:shortUrl", async (req, res) => {
 
   //Redirect to the longUrl
   res.redirect(shortUrl.longUrl);
+});
+
+app.post("/register", async (req, res) => {
+  const {username, email, password}  = req.body
+  if(!email || !password){
+    return res.sendStatus(400).send("Faltam elementos para que o cadastro seja completo")
+  }
+
+  try {
+    // US2 - O banco de dados deve armazenar senhas criptografadas e garantir a unicidade dos e-mails
+    // Verificar se o e-mail já existe
+    const usuarioExistente = await User.findOne({ email });
+    if (usuarioExistente) {
+      return res.status(409).json({ error: "E-mail já cadastrado" });
+    }
+
+    // Criptografar a senha
+    // Gerar o hash da senha
+    const saltRounds = 2; // Número de rounds de hashing (mais alto = mais seguro, mas mais lento)
+    const criptoPassword = await bcrypt.hash(password, saltRounds);
+
+    // Salvar no banco de dados
+    await User.create({ name: username, email, password: criptoPassword });
+
+    return res.status(201).json({ message: "Saved with success" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(3080, () => {
