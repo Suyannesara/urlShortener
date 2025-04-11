116f03274e178f777dc5be7b9ea459f5c355af94
suyanne <suyannesara17@gmail.com>
2025-04-11 17:19:47 -0300

    any change to deploy

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -9,7 +9,7 @@ router.beforeEach((to, from, next) => {
     const isAuthenticated = !!token;
   
     if (to.meta.requiresAuth && !isAuthenticated) {
-      next("/login"); // redireciona pra login se não tiver logado
+      next("/login");
     } else {
       next();
     }
