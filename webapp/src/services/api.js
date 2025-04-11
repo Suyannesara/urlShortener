9a889462aeb55f4b750b4ed6695c60173622d432
suyanne <suyannesara17@gmail.com>
2025-04-11 18:29:58 -0300

    Vue variable

--- webapp/src/services/api.js
+++ webapp/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: process.env.VUE_API_URL
 });
 
 http.interceptors.request.use((config) => {
